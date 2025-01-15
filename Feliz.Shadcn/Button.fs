ca78be9609bc50f3182f6126b01ad79805ce3f6d
cabboose <shayanftw@gmail.com>
2025-01-15 08:42:00 +0800

    Add some explanatory docs to Button for the component structures

--- Feliz.Shadcn/Button.fs
+++ Feliz.Shadcn/Button.fs
@@ -3,18 +3,23 @@ module Feliz.Shadcn.Button
 
 open Feliz.Interop.Extend
 open Fable.Core
-open Fable.Core.JsInterop
 open Feliz
 
-emitJsStatement () """import { Slot } from "@radix-ui/react-slot" """
 JSX.injectLib
 
 // --------------- Button -------------- //
+/// Shadcn Button prop interface type
 type [<Erase>] IButtonProp = interface end
+/// Accessor for button properties
 type [<Erase>] button =
+    // Inherit Feliz base properties
     inherit prop<IButtonProp>
+    // Definitions for library defined properties
     static member inline asChild ( value : bool ) : IButtonProp = Interop.mkProperty "asChild" value
 
+/// Button variants as a `Class-Variance-Authority` object.
+/// You can modify existing variants or add your own.
+/// After adding your own variants, it's suggested to create a variant enum prop. Follow the examples in the `button.variant` type.
 let buttonVariants = JSX.jsx """
 import { cva } from "class-variance-authority";
 cva(
@@ -47,9 +52,11 @@ cva(
   }
 )
 """
-    
+
+/// Accessor for button enums.
 [<RequireQualifiedAccess>]
 module [<Erase>] button =
+    /// ButtonVariant enumerators
     type [<Erase>] variant =
         static member inline default' : IButtonProp = Interop.mkProperty "variant" "default"
         static member inline destructive : IButtonProp = Interop.mkProperty "variant" "destructive"
@@ -57,14 +64,25 @@ module [<Erase>] button =
         static member inline secondary : IButtonProp = Interop.mkProperty "variant" "secondary"
         static member inline ghost : IButtonProp = Interop.mkProperty "variant" "ghost"
         static member inline link : IButtonProp = Interop.mkProperty "variant" "link"
+    /// ButtonVariant enumerators
     type [<Erase>] size =
         static member inline default' : IButtonProp = Interop.mkProperty "size" "default"
         static member inline sm : IButtonProp = Interop.mkProperty "size" "sm"
         static member inline lg : IButtonProp = Interop.mkProperty "size" "lg"
         static member inline icon : IButtonProp = Interop.mkProperty "size" "icon"
 
+/// The Button JSX Element definition
+/// To use this is in your own JSX, you would have to ensure Fable imports the type using a dummy binding
+/// <code>
+/// let _ = Button
+/// </code>
+/// To use in Feliz style, access via `Shadcn`.
+/// <code>
+/// Shadcn.Button [
+///   // add props
+/// ]
+/// </code>
 let Button : JSX.ElementType = JSX.jsx """
-import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
   const Comp = asChild ? Slot : "button"
@@ -82,3 +100,4 @@ type [<Erase>] Shadcn =
   static member inline Button ( props : IButtonProp list ) = JSX.createElement Button props
   static member inline Button ( children : ReactElement list ) = JSX.createElementWithChildren Button children
   static member inline Button ( value : string ) = JSX.createElement Button [ button.text value ]
+  static member inline Button ( el : ReactElement ) = JSX.createElement Button [ button.asChild true ; button.children [ el ] ]
